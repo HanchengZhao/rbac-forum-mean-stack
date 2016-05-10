8e448abe2bcd7653b3413baf98c7e8c8e07f9957
hancheng <chancidy@gmail.com>
2016-05-10 00:49:14 +0000

    Romove not-used folder

--- not-used/hash.php
+++
@@ -1,8 +0,0 @@
-<?php
- $admin = hash('sha256',admin);
- $moderator = hash('sha256',moderator);
- $user = hash('sha256',user);
- echo "admin-hash:$admin\r\n";
- echo "moderator-hash:$moderator\r\n";
- echo "user-hash:$user\r\n";
-?>
